1b17a48e9bb49f79adee8566820ae3d406b3ae7f
prog <prog@mail.ru>
2024-10-30 13:40:36 +0300

    Добавить ТРИГГЕРЫ

--- TASK4/TRIGGERS.sql
+++ TASK4/TRIGGERS.sql
@@ -1,32 +1,6 @@
 -- https://wiki.livid.pp.ru/students/dbms/lectures/7.html
-CREATE OR REPLACE FUNCTION check_free_rooms()
-   RETURNS TRIGGER
-   LANGUAGE PLPGSQL
-AS $$
-	DECLARE	 rooms_count integer;
-BEGIN
-   SELECT count(*) 
-   INTO rooms_count
-   FROM rooms r JOIN room_status rs ON r.room_status_id = rs.room_status_id 
-   WHERE rs.title = 'Чистый'; 
-   IF rooms_count = 0 THEN
-   		RAISE SQLSTATE '45000' USING MESSAGE = 'ALL ROOMS RESERVED';
-   	
-   END IF;
-   RETURN NULL;
 
-END;
-$$
-
-
-CREATE OR REPLACE TRIGGER check_rooms
-BEFORE INSERT ON bookings 
-EXECUTE FUNCTION check_free_rooms();
-
-
-INSERT INTO bookings(client_id, date_start, date_end, room_id) VALUES (1, '2024-11-01', '2024-11-05',6); 
-
-
+--- триггерная функция  которая меняет статус комнаты 
 CREATE OR REPLACE FUNCTION reserve_room()
    RETURNS TRIGGER
    LANGUAGE PLPGSQL
@@ -40,10 +14,48 @@ BEGIN
 END;
 $$
 
+--- триггер срабатывает при добавлении записи в таблицу bookings
 CREATE OR REPLACE TRIGGER set_reserve_rooms
 AFTER INSERT ON bookings
 FOR EACH ROW 
 EXECUTE FUNCTION reserve_room();
+
+
+--- Триггерная функция, которая проверяет есть ли доступные комнаты
+-- если пустых комнат нет, то то генерируется ошибка с кодом 45000 и сообщением
+--- свободных комнат нет
+CREATE OR REPLACE FUNCTION check_free_rooms()
+   RETURNS TRIGGER
+   LANGUAGE PLPGSQL
+AS $$
+	DECLARE	 rooms_count integer;
+BEGIN
+-- подсчитываем количество свободных комнат(комнаты, в которые можно заселить имеют статус Чистый) 
+   SELECT count(*) 
+   INTO rooms_count
+   FROM rooms r JOIN room_status rs ON r.room_status_id = rs.room_status_id 
+   WHERE rs.title = 'Чистый'; 
+--- если чистых комнат нет, то генерируется ошибка с кодом 45000 и сообщением
+--- свободных комнат нет
+   IF rooms_count = 0 THEN
+   		RAISE SQLSTATE '45000' USING MESSAGE = 'ALL ROOMS RESERVED';
+   	
+   END IF;
+   ---
+   RETURN NULL;
+
+END;
+$$
+
+--- Операторный триггер, вызывается единожды при попытке добавить запись в bookings
+CREATE OR REPLACE TRIGGER check_rooms
+BEFORE INSERT ON bookings 
+EXECUTE FUNCTION check_free_rooms();
+
+
+ 
+
+
 
 
 CREATE OR REPLACE FUNCTION is_current_room_free()
@@ -68,6 +80,9 @@ BEFORE INSERT ON bookings
 FOR EACH ROW 
 EXECUTE FUNCTION is_current_room_free();
 
+
+
+INSERT INTO bookings(client_id, date_start, date_end, room_id) VALUES (1, '2024-11-01', '2024-11-05',6);
 
 
 
